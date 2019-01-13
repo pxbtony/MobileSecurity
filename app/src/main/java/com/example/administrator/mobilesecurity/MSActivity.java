2ed11c0767f1339cb65fa93a7f41aab5aadd02e1
pxbtony <pxbnameistony@163.com>
2019-01-13 12:58:29 +0800

    test

--- app/src/main/java/com/example/administrator/mobilesecurity/MSActivity.java
+++ app/src/main/java/com/example/administrator/mobilesecurity/MSActivity.java
@@ -3,7 +3,7 @@ package com.example.administrator.mobilesecurity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MSActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/administrator/mobilesecurity/aa.java
@@ -0,0 +1,4 @@
+package com.example.administrator.mobilesecurity;
+
+public class aa {
+}
